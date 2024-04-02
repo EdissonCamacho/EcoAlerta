e266df7afa67bae5e3947a1acc48a4d8b1a94c34
Edisson Camacho <eacamacho46@misena.edu.co>
2024-04-01 22:15:45 -0500

    NUEVO 2

---
+++ ecoalertaBANCK/src/main/java/com/ecoalerta/EcoalertaApplication.java
@@ -0,0 +1,13 @@
+package com.ecoalerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcoalertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcoalertaApplication.class, args);
+	}
+
+}
