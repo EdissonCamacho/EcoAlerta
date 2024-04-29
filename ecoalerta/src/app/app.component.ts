de4ed84a41e47cac1a07001e1767c852e29cb01b
dbarahona@qvision.com.co <dbarahona@qvision.com.co>
2024-04-28 21:16:23 -0500

    revision

--- ecoalerta/src/app/app.component.ts
+++ ecoalerta/src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { CarruselComponent } from './components/carrusel/carrusel.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CarruselComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
