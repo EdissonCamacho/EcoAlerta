b5c1185aea64adcc9a9b77511cd4d4c7629cf2bb
LorenaBarr <karenlbarrera@gmail.com>
2024-05-12 19:45:20 -0500

    color nav

--- ecoalerta/src/app/app.component.ts
+++ ecoalerta/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent, CarruselComponent,FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, CarruselComponent, NavComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
