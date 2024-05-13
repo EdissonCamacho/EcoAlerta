7d6551505c01d0722341a40336077856e31dca0d
LorenaBarr <karenlbarrera@gmail.com>
2024-05-12 21:34:01 -0500

    Nav, carrsuel y seccion texto, carrusel sin cargar imagenes

--- ecoalerta/src/app/app.component.ts
+++ ecoalerta/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
+import { SeccionComponent } from './components/seccion/seccion.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CarruselComponent, NavComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent, CarruselComponent, SeccionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
