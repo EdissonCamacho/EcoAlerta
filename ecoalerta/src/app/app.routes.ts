f62f1c0cc4375fd2e06a3f943047b88e157387d2
LorenaBarr <karenlbarrera@gmail.com>
2024-05-31 22:04:33 -0500

    componente login y ruta de login y register

--- ecoalerta/src/app/app.routes.ts
+++ ecoalerta/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { CarruselComponent } from './components/carrusel/carrusel.component';
 import { PaginationComponent } from './components/pagination/pagination.component';
+import { LoginComponent } from './pages/login/login.component'; 
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
     {
@@ -8,6 +10,8 @@ export const routes: Routes = [
     },
     {path:"referenciasbibliograficas/:id",component:PaginationComponent},
     {path:"impactosambientales/:id",component:PaginationComponent},
+    {path:"login",component:LoginComponent},
+    {path:"register",component:RegisterComponent},
 
 
 ];
